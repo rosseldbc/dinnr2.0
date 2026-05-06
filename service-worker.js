31d52c13d767e4364d7c39f88e0c41ce4d91957d
rosseldbc <rosseldbc@gmail.com>
2026-05-06 22:26:24 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'dinnr-v1';
+const CACHE = 'dinnr-v2';
 const ASSETS = ['/', '/index.html', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
